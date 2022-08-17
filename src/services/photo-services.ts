c046872865c55b84da16390d71f1384a1544b75d
Gavin Anderson <btcgrowth1@gmail.com>
2022-08-17 12:03:51 -0400

    added create and update

--- src/services/photo-services.ts
+++ src/services/photo-services.ts
@@ -1,10 +1,11 @@
+import { ObjectId } from "mongodb";
 import { db } from "../db/db";
 import { Photo } from "../models/photo";
 
 interface PhotoServices {
     updateLikes(id:string, inc:number): Promise<Photo>;
+    // createComment(id:string, comment:string): Promise<Photo>;
     createPhoto(photo:Photo): Promise<string>;
-    createComment(id:string, comment:string): Promise<Photo>;
     getAllPhotos(): Promise<Photo[]>
 }
 
@@ -15,3 +16,34 @@ export const getAllPhotos = async (): Promise<Photo[]> => {
 
     return photos;
 };
+
+export const createPhoto = async (photo: Photo): Promise<string> => {
+    try{
+        const res = await photoCollections.insertOne(photo);
+        return res.insertedId.toString();
+    }catch (error) {
+        return 'Something went wrong'
+    }
+}
+
+export const updateLikes = async (
+    id:string,
+     inc: number = 1
+     ): Promise<Photo> => {
+    const res = await photoCollections.findOneAndUpdate(
+      {_id: new ObjectId(id)},
+      {$inc: {likes: inc}}
+    );
+
+    const updatedPhoto = res.value as Photo
+    
+    return updatedPhoto
+}
+
+export const photoServices: PhotoServices = {
+    getAllPhotos,
+    createPhoto,
+    updateLikes, 
+    // createComment
+}
+    
